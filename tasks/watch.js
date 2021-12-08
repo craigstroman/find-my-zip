8666922a2c4acb8a0f5c83b768e9bf693e7f70cc
Craig Stroman <cstroman@gmail.com>
2021-12-07 22:02:45 -0500

    Deleting files

--- tasks/watch.js
+++
@@ -1,6 +0,0 @@
-import gulp from 'gulp';
-
-gulp.task('watch', () => {
-    gulp.watch( './src/css/scss/**/*.scss', ['build-css']);
-    gulp.watch(['./src/js/app.js', '!./node_modules/**'], ['lint-js', 'webpack']);
-});
